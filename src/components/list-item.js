0418f59f7aa27bf9d27fa4918183429383e6c6fc
Maxim Siebert <maxim.siebert@gmail.com>
2019-07-18 23:16:53 -0400

    dogs but no search rip

--- src/components/list-item.js
+++ src/components/list-item.js
@@ -3,23 +3,19 @@ import axios from "axios";
 
 const ListItem = (props) => {
     const [image, setImage] = useState('')
-    console.log('props', props.breed);
 
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        apiUrl = 'https://api.thedogapi.com/v1/breeds',
-        apiImageUrl = 'https://api.thedogapi.com/v1/images/search',
-        apiKey = 'dda2eda4-5ec5-46d6-850f-ca0619ad7dd1';
+          apiImageUrl = proxyUrl + 'https://api.thedogapi.com/v1/images/search',
+          apiKey = 'dda2eda4-5ec5-46d6-850f-ca0619ad7dd1';
 
-    const getDoggoImage = async () => {
-        const dogImage = await axios.get(proxyUrl + apiImageUrl, {
+    const getDogImage = async () => {
+        const dogImage = await axios.get(apiImageUrl, {
             params: {
-                limit: null,
+                limit: 1,
                 apiKey: apiKey,
                 breed_id: props.breed.id,
             }
         });
-
-        console.log('dogs', dogImage)
 
         let url = ""
 
@@ -27,24 +23,23 @@ const ListItem = (props) => {
             url = dogImage.data[0].url
         }
 
-        console.log('url', url)
         setImage(url);
     }
 
     useEffect(() => {
-        getDoggoImage();
+        getDogImage();
     },[]);
 
 
     return (
         <li className={"w-1/4 px-4 mb-8"}>
             <div className={"overflow-hidden rounded-lg mb-2 h-48 bg-gray-200"}>
-                { props.breed.image !== "" &&
-                    <img
-                        className={"h-64 w-full object-cover"}
-                        src={image}
-                        alt={"Picture of a " + props.breed.name}
-                    />
+                {image !== "" &&
+                <img
+                    className={"h-64 w-full object-cover fade"}
+                    src={image}
+                    alt={"Picture of a " + props.breed.name}
+                />
                 }
             </div>
             <h3 className={"font-bold"}>{props.breed.name}</h3>
--- src/components/list.js
+++ src/components/list.js
@@ -7,44 +7,44 @@ import Search from "./search";
 const List = () => {
 
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-        apiUrl = 'https://api.thedogapi.com/v1/breeds',
-        apiImageUrl = 'https://api.thedogapi.com/v1/images/search',
-        apiKey = 'dda2eda4-5ec5-46d6-850f-ca0619ad7dd1';
-
+          apiUrl = proxyUrl + 'https://api.thedogapi.com/v1/breeds',
+          apiKey = 'dda2eda4-5ec5-46d6-850f-ca0619ad7dd1';
 
     const [loading, setLoading] = useState(true);
-    const [breeds, setBreeds] = useState([]);
-    const [error, setError] = useState(null);
+    const [error, setError] = useState(false);
     const [pageTitle, setPageTitle] = useState("All dogs");
     const [query, setQuery] = useState("");
+    const [allDogs, setAllDogs] = useState([]);
     const [filteredResults, setFilteredResults] = useState([]);
 
+    let dogs;
 
     const workflow = async () => {
-      const allDogs = await axios.get(proxyUrl + apiUrl, {
-          params: {
-              limit: null,
-              apiKey: apiKey,
-          }
-      });
-      console.log('allDogs', allDogs.data);
+        try {
+            const allDogs = await axios.get(apiUrl, {
+                params: {
+                limit: null,
+                apiKey: apiKey,
+                }
+            });
 
-      const allDogsIds = allDogs.data.map(d => { return d.id });
-      console.log('allDogsIds', allDogsIds)
+            dogs = allDogs.data.map(d => {
+                return {
+                    id: d.id,
+                    name: d.name,
+                }
+            });
 
-      let dogs = allDogs.data.map(d => {
-          return {
-              id: d.id,
-              name: d.name,
-          }
-      })
-      console.log('dogs', dogs)
+            setAllDogs(dogs);
+            setFilteredResults(dogs);
+            setLoading(false);
+        } catch {
+            setError(true);
+            setLoading(false);
 
+        }
 
-    console.log('dogs', dogs)
-    setFilteredResults(dogs);
-    setLoading(false);
-    };
+    }
 
 
 
@@ -53,36 +53,26 @@ const List = () => {
     },[]);
 
 
-    // useEffect(() => {
-    //     setFilteredResults(breeds)
-    //     console.log('filter', filteredResults)
-    //     if (query !== "") {
-    //         setFilteredResults(
-    //             breeds.filter(breed =>
-    //                 breed.name.toLowerCase().includes(query.toLowerCase())
-    //             )
-    //         );
-    //     } else {
-    //         setFilteredResults(breeds)
-    //     }
-    // }, [query]);
+    useEffect(() => {
 
-    // useEffect(() => {
-        // if (query !== "") {
-        //     setPageTitle(filteredResults.length + ' dogs')
-        // } else {
-        //     setPageTitle('All dogs')
-        // }
-    // }, [filteredResults]);
+    }, [query]);
+
+    useEffect(() => {
+        if (query !== "") {
+            setPageTitle(filteredResults.length + ' dogs')
+        } else {
+            setPageTitle('All dogs')
+        }
+    }, [filteredResults]);
 
 
     const ListItems = () => {
         return (
             filteredResults
-                .map((breed) => (
+                .map((dog) => (
                     <ListItem
-                        key={breed.id}
-                        breed={breed}
+                        key={dog.id}
+                        breed={dog}
                     />
                 ))
         )
@@ -90,7 +80,7 @@ const List = () => {
 
     return (
         <div className={"w-4/5 relative"}>
-            <div className={"flex items-center sticky top-0 left-0 right-0 px-8 py-4 bg-white"}>
+            <div className={"flex items-center sticky top-0 left-0 right-0 px-8 py-4 bg-white z-10"}>
                 <h1 className={"text-2xl font-bold"}>
                     {pageTitle}
                 </h1>
@@ -99,7 +89,7 @@ const List = () => {
                     queryChange={setQuery}
                 />
             </div>
-            {!loading && filteredResults.length > 0 ? (
+            {!loading ? (
                 <ul className={"flex flex-wrap py-8 px-4"}>
                     <ListItems />
                 </ul>
