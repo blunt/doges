d4410c3457fa06dba3e36ab4aa95209410a7d39d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-23 02:14:48 +0000

    Deploy blunt/doges to github.com/blunt/doges.git:master

---
+++ precache-manifest.c3a1122d25b0eada1c925d92d30b60d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62ba4001268b5ee5738d786231b66ce",
+    "url": "/doges/index.html"
+  },
+  {
+    "revision": "1340c367845755b37e80",
+    "url": "/doges/static/css/main.3a0f22ad.chunk.css"
+  },
+  {
+    "revision": "ce4547ceab11a5f9fed9",
+    "url": "/doges/static/js/2.6d6f507a.chunk.js"
+  },
+  {
+    "revision": "1340c367845755b37e80",
+    "url": "/doges/static/js/main.ae0a563d.chunk.js"
+  },
+  {
+    "revision": "9dcdb6754632fe39bf2c",
+    "url": "/doges/static/js/runtime~main.16dcfd3d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1c3624391c9e10555f9de7c421c55a6.js"
+  "/doges/precache-manifest.c3a1122d25b0eada1c925d92d30b60d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doges/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
