bf8246137d215c67550bfbc236c3f59c38fa9639
Maxim Siebert <maxim.siebert@gmail.com>
2019-07-22 22:42:06 -0400

    Updates

---
+++ precache-manifest.f0a709edfccb45b2c4f80df49f743154.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af735e8d68f284dcd3280b20a9df2371",
+    "url": "/doges/index.html"
+  },
+  {
+    "revision": "1d045a41660816b32951",
+    "url": "/doges/static/css/main.3a0f22ad.chunk.css"
+  },
+  {
+    "revision": "ce4547ceab11a5f9fed9",
+    "url": "/doges/static/js/2.6d6f507a.chunk.js"
+  },
+  {
+    "revision": "1d045a41660816b32951",
+    "url": "/doges/static/js/main.e7fc8dda.chunk.js"
+  },
+  {
+    "revision": "9dcdb6754632fe39bf2c",
+    "url": "/doges/static/js/runtime~main.16dcfd3d.js"
+  }
+]);
